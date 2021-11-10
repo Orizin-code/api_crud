fd21a2526e17fea0c7b18f49310c1e0614f2e49b
Orizin <nlgporizin10@gmail.com>
2021-11-11 01:05:52 +0900

    テストコード/show

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -1,14 +1,24 @@
 class V1::PostsController < ApplicationController
+  before_action :set_post, only: %i[show update destroy]
+
   def index
     posts = Post.all
     render json: posts
   end
 
-  def show; end
+  def show
+    render json: @post
+  end
 
   def create; end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
