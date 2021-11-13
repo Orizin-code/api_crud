989403ac285eb1a9ef2c070a567bc9917fffedff
Orizin <nlgporizin10@gmail.com>
2021-11-13 23:09:15 +0900

    テストコード/create

--- app/controllers/v1/posts_controller.rb
+++ app/controllers/v1/posts_controller.rb
@@ -10,7 +10,15 @@ class V1::PostsController < ApplicationController
     render json: @post
   end
 
-  def create; end
+  def create
+    post = Post.new(post_params)
+
+    if post.save
+      render json: post
+    else
+      render json: post.errors, status: :unprocessable_entity
+    end
+  end
 
   def update; end
 
@@ -20,5 +28,9 @@ class V1::PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
